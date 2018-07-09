8a1afe090c4b886c0849eafb0f8b6c3078062ced
sunleo813 <sunleo@livemail.tw>
2018-07-09 17:44:40 +0800

    Change alipayAPI.js

--- alipayAPI.js
+++ alipayAPI.js
@@ -4,23 +4,25 @@ var crypto = require('crypto');
 var request = require('request');
 
 
+
 var accountSettings = {
     //APP_ID: '2018062660487015',
     APP_ID: '2016091400507437',
     APP_GATEWAY_URL: 'http://ec2-34-219-4-10.us-west-2.compute.amazonaws.com/alipay/pay_notice.js',
+    //APP_GATEWAY_URL: 'http://www.alipay.com',
     //APP_PRIVATE_KEY_PATH: './pem/Alipay_App_Prikey2048.pem',
-    APP_PRIVATE_KEY_PATH: './pem/Alipay_App_Prikey2048.pem',
+    APP_PRIVATE_KEY_PATH: './pem/AppPrivateKey.pem',
     //ALIPAY_GATEWAY: 'https://openapi.alipay.com/gateway.do?',
     ALIPAY_GATEWAY: 'https://openapi.alipaydev.com/gateway.do?',
-    SERVICE_AMT: 15
+    SERVICE_AMT: '15.00'
 };
 
 //gen Biz_Content
 function BizContentBuilder(tradeNo) {
     var bizContent = {
-        subject: 'EasyTech Auto Washer Service',
+        subject: 'EasyTech',
         out_trade_no: tradeNo,
-        total_amount: accountSettings.SERVICE_AMT.stringify,
+        total_amount: accountSettings.SERVICE_AMT,
         qr_code_timeout_express: '30m'
     };
     return JSON.stringify(bizContent);
@@ -38,14 +40,16 @@ function sendAlipayOrder(signedTrans) {
 }
 
 
-function BuildSignedTrans(paramMap) {
-    var paramList = [...paramMap].filter(([k1, v1]) => k1 !== 'sign' && v1);
-    paramList.sort();
-    var paramsString = paramList.map(([k, v]) => `${k}=${v}`).join('&');
-    console.log(paramsString);
+function BuildSignedTrans(signType, paramsString) {
+    // var paramList = [...paramMap].filter(([k1, v1]) => k1 !== 'sign' && v1);
+    // //console.log(paramList);
+    // paramList.sort();
+    // console.log(paramList);
+    // var paramsString = paramList.map(([k, v]) => `${k}=${v}`).join('&');
+    // console.log(paramsString);
     var privateKey = fs.readFileSync(accountSettings.APP_PRIVATE_KEY_PATH, 'utf8');
     //console.log(privateKey);
-    var signType = paramMap.get('sign_type');
+    //var signType = paramMap.get('sign_type');
     return SignWithPrivateKey(signType, paramsString, privateKey);
 }
 
@@ -56,7 +60,7 @@ function SignWithPrivateKey(signType, content, privateKey) {
     var sign;
     if (signType.toUpperCase() === 'RSA2') {
         sign = crypto.createSign("RSA-SHA256");
-        //     console.log('SignType: RSA2');
+        console.log('SignType: RSA2');
     } else if (signType.toUpperCase() === 'RSA') {
         sign = crypto.createSign("RSA-SHA1");
         //      console.log('SignType: RSA-SHA1');
@@ -71,18 +75,32 @@ function SignWithPrivateKey(signType, content, privateKey) {
 genSignedOrder = function (outTradeNo) {
     var params = new Map();
     params.set('timestamp', moment().format('YYYY-MM-DD HH:mm:ss'));
+    //params.set('timestamp', '2018-07-09 13:46:15');
     params.set('method', 'alipay.trade.precreate');
     params.set('app_id', accountSettings.APP_ID);
     params.set('sign_type', 'RSA2');
     params.set('version', '1.0');
-    params.set('notify_url', accountSettings.APP_GATEWAY_URL);
+   // params.set('notify_url', accountSettings.APP_GATEWAY_URL);
     params.set('charset', 'utf-8');
     params.set('biz_content', BizContentBuilder(outTradeNo));
-    params.set('sign', BuildSignedTrans(params));
-    //return [...params].map((k,v)=>`${k}=${encodeURIComponent(v)}`).join('&');
-    var resultString = [...params].map(([k, v]) => `${k}=${v}`).join('&');
-    resultString=encodeURI(resultString);
-    //console.log(resultString);
+
+    //sort params and clear empty field
+    var paramList = [...params].filter(([k1, v1]) => k1 !== 'sign' && v1);
+    //console.log(paramList);
+    paramList.sort();
+    //console.log(paramList);
+    var paramsString = paramList.map(([k, v]) => `${k}=${v}`).join('&');
+    console.log(paramsString);
+    var sign=BuildSignedTrans(params.get('sign_type'),paramsString);
+    paramsString=paramsString+'&sign='+sign;
+    //params.set('sign', BuildSignedTrans(params.get('sign_type'),parpamsString));
+    
+    //var resultString= [...params].map((k,v)=>`${k}=${encodeURIComponent(v)}`).join('&');
+    //var resultString = [...params].map(([k, v]) => `${k}=${v}`).join('&');
+    //var resultString= [...params].map((k,v)=>`${urlencode(`${k}=${v}`)}`).join('&');
+    //resultString=encodeURI(resultString);
+    resultString=encodeURI(paramsString);
+    console.log(resultString);
     return resultString;
 
 };
