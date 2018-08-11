858583d3c988bf652ec5cb6f959710de523d69e6
sunleo813 <sunleo@livemail.tw>
2018-08-12 07:15:06 +0800

    Add requestSender class and test case, but has bug when run mocha

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,8 @@
 var assert = require('chai').assert;
 var Content = require('../js/content');
 var AliPrecreateContent = require('../js/content');
+var AlipayRequestSender = require('../js/requestSender');
+var moment = require('moment');
 
 describe('Content', function () {
     describe('#constructor()', function () {
@@ -20,19 +22,62 @@ describe('AliPrecreateContent', function () {
     })
     describe('#createParams', function () {
         it('method should return params from both Content & AliprecreateContent', function () {
-            let ali = new AliPrecreateContent('ZUH-001',20);
+            let ali = new AliPrecreateContent('ZUH-001', 20);
             let params = ali.createParams();
             assert.equal('alipay.trade.precreate', params.get('method'));
             assert.equal('RSA2', params.get('sign_type'));
         })
     })
-    describe('#build', function(){
-        it('show paramsString after execute function build', function(){
-            let ali=new AliPrecreateContent('ZUH-001',20);
-            let paramString=ali.build();
+    describe('#build', function () {
+        it('show paramsString after execute function build', function () {
+            let ali = new AliPrecreateContent('ZUH-001', 20);
+            let paramString = ali.build();
             console.log(paramString);
 
 
         })
     })
+})
+
+describe('AlipayRequestSender', function () {
+    var paramsString;
+    var ali;
+
+    before(function () {
+        let id = 'ZUH-000-' + moment().format('YYYYMMDDHHmmss');
+        let aliContent = new AliPrecreateContent(id);
+        paramsString = aliContent.build();
+        ali = new AlipayRequestSender(paramsString, 'alipay.trade.precreate');
+    })
+    describe('#constructor', function () {
+        it('check properties after constructor execute', function () {
+            assert.equal("alipay.trade.precreate", ali.methodType)
+            assert.equal(paramsString, ali.paramString);
+            console.log(ali.alipayUrl)
+        })
+        it('All variables in sender should work', function (done) {
+            console.log('ali value= ' + JSON.stringify(ali));
+            console.log("paramsString value: " + paramsString);
+            ali.sendRequest(function(result){
+                assert.equal('Succe', result.msg);
+                done();
+            })
+        })
+    })
+
+    // describe('#sendRequest', function (done) {
+
+    //         it('All variables in sender should work', function () {
+    //             console.log('ali value= ' + JSON.stringify(ali));
+    //             console.log("paramsString value: " + paramsString);
+
+
+    // ali.sendRequest(function(result){
+
+    //     assert.equal('Success',result.msg);
+    //     done();
+    //         // })
+    //     })
+
+    //  })
 })
